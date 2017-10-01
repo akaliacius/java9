89d95f0415368deccfdddcac1df4763af52a8349
Andrius K <a.kaliacius@gmail.com>
2017-10-01 17:43:37 +0100

    first commit

---
+++ firstmodule/src/firstmodule/com/codeminister/firstmodule/FirstModule.java
@@ -0,0 +1,8 @@
+package com.codeminister.firstmodule;
+
+public class FirstModule{
+    public static void main(String[] args){
+        Greeting greeting = new Greeting();
+        System.out.println(greeting.greet());
+    }
+}
---
+++ firstmodule/src/firstmodule/com/codeminister/firstmodule/Greeting.java
@@ -0,0 +1,7 @@
+package com.codeminister.firstmodule;
+
+public class Greeting{
+    String greet(){
+        return "Hello from Java 9 module!";
+    }
+}
